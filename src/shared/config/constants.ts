03214cd5553495db848a303b1f89e4173f79f734
Frederick Boyd <frederickboyd255@gmail.com>
2025-09-24 09:10:17 -0500

    added some projects list

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -145,4 +145,51 @@ export const projects: ProjectTypes[] = [
     thumnail: "tech-note.jpg",
     featured: false,
   },
+  {
+    title: "WhatsApp AI Chatbot Flow Builder",
+    description:
+      "The WhatsApp AI Chatbot Flow Builder lets businesses automate chat flows using a drag-and-drop UI. It supports editable questions, dynamic responses, and Twilio integration. Clean UI, easy setup, and smooth automation.",
+    code: "https://github.com/Saini-Yogesh/WhatsApp-AI-Chatbot",
+    tech: [
+      "React",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "Twilio API",
+      "OpenAI API",
+    ],
+    live: "https://whatsapp-ai-chatbot.vercel.app/",
+    thumnail: "Flow-Builder.61f63a3f818087a33578.png",
+    featured: false,
+  },
+  {
+    title: "Trip-connect",
+    description:
+      "Trip-Connect is a platform that connects solo travelers to share expenses and enhance social interactions. Built with React.js, Node.js, Express.js, and MongoDB, it offers a responsive UI and real-time travel planning features for a seamless experience.",
+    code: "https://github.com/Saini-Yogesh/Trip-connect",
+    tech: ["React", "Node.js", "Express", "MongoDB", "JWT"],
+    live: "https://saini-yogesh.github.io/Trip-connect/",
+    thumnail: "TripConnect.91abd21a463ee95115cc.png",
+    featured: false,
+  },
+  {
+    title: "Segment Tree Visualizer",
+    description:
+      "The Segment Tree Visualizer is a web-based application built using Next.js that provides an interactive way to understand and visualize different types of segment trees. This project showcases efficient data structure operations and dynamic visualizations.",
+    code: "https://github.com/Saini-Yogesh/segment-tree-visualization",
+    tech: ["Next.js", "D3.js", "JavaScript"],
+    live: "https://segment-tree-visualization.vercel.app/",
+    thumnail: "SGT.fc0d5b4ce6e13cb54e58.png",
+    featured: false,
+  },
+  {
+    title: "E-Commerce-Store",
+    description:
+      "E-Commerce Website is a modern shopping platform built with React, Node.js, Express.js, and MongoDB. It features user authentication, product browsing, shopping cart, secure payments (PayPal/Stripe), and an admin dashboard for managing inventory and orders. With a responsive design and seamless experience, it ensures smooth transactions.",
+    code: "https://github.com/Saini-Yogesh/E-Commerce-Store",
+    tech: ["React", "Node.js", "Express", "MongoDB", "Tailwind CSS"],
+    live: "https://github.com/Saini-Yogesh/E-Commerce-Store",
+    thumnail: "E-commerce-store.442c0760eac36617fab8.png",
+    featured: false,
+  },
 ];
